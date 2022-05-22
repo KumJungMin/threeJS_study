7319fcae1202423bf5b7c6513e588b218bfe953f
KumJungMin <rmawjdals@naver.com>
2022-05-22 17:20:37 +0900

    배경색 조절하기 - render에서 조절하는 법

---
+++ 02/basic/src/ex03.js
@@ -0,0 +1,51 @@
+// 주제: 배경색, 투명도 설정
+import * as THREE from "three";
+
+export default function example() {
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGL1Renderer({
+    canvas,
+    antialias: true,
+    alpha: true, // 배경색 투명하게
+  });
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x00ff00); // 배경색 지정
+  renderer.setClearAlpha(0.5); // 투명도 조절 - 0 ~ 1사이의 값
+
+  const scene = new THREE.Scene();
+
+  // camera
+  const camera = new THREE.PerspectiveCamera(
+    75, // 시아갹
+    window.innerWidth / window.innerHeight, // 종횡비(aspect)
+    0.1, // near
+    1000 // far
+  );
+
+  camera.position.x = 1;
+  camera.position.y = 2;
+  camera.position.z = 5;
+  scene.add(camera);
+
+  // mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const meterial = new THREE.MeshBasicMaterial({
+    color: "red",
+  });
+  const mesh = new THREE.Mesh(geometry, meterial);
+  scene.add(mesh);
+
+  // 그리기
+  renderer.render(scene, camera);
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix(); // 카메라 투영에 관련한 값에 변화를 줄 시 꼭 실행시켜줘야함
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+}
--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -1,4 +1,5 @@
 // import example from "./ex01.js";
-import example from "./ex02.js";
+// import example from "./ex02.js";
+import example from "./ex03.js";
 
 example();
