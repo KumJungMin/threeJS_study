b95c8082faf436a8db7e5b75ab8ff12d036c2272
KumJungMin <rmawjdals@naver.com>
2022-05-22 16:06:56 +0900

    랜더러 - html canvas 방식으로 랜더러 설정하기

--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -2,6 +2,6 @@
 import * as THREE from "three";
 
 // 2. 랜더러 생성
-const renderer = new THREE.WebGL1Renderer();
+const canvas = document.querySelector("#three-canvas");
+const renderer = new THREE.WebGL1Renderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight); // 랜더러 크기 설정
-document.body.appendChild(renderer.domElement); // 랜더러가 가진 canvas요소(renderer.domElement)를 body에 자식으로 추가
