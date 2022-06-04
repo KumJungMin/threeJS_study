d2dc2b64c3aaf7f59f42e0aca951c005f1926413
KumJungMin <rmawjdals@naver.com>
2022-06-04 15:35:49 +0900

    chore: add ex04.js

--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -1,5 +1,6 @@
 // import example from "./ex01.js";
 // import example from "./ex02.js";
-import example from "./ex03.js";
+// import example from "./ex03.js";
+import example from "./ex04.js";
 
 example();
