41875c1353b6b964ffe968b02e7dfd648a320c0a
KumJungMin <rmawjdals@naver.com>
2022-06-09 21:30:12 +0900

    축, 그리드 헬퍼 사용해보기

--- 03/utility/src/ex01.js
+++ 03/utility/src/ex01.js
@@ -1,64 +1,81 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-// ----- 주제: AxesHelper, GridHelper
+// ----- 주제: AxesHelper(축), GridHelper(그리드)
 
 export default function example() {
-	// Renderer
-	const canvas = document.querySelector('#three-canvas');
-	const renderer = new THREE.WebGLRenderer({
-		canvas,
-		antialias: true
-	});
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
-	// Scene
-	const scene = new THREE.Scene();
+  // Scene
+  const scene = new THREE.Scene();
 
-	// Camera
-	const camera = new THREE.PerspectiveCamera(
-		75,
-		window.innerWidth / window.innerHeight,
-		0.1,
-		1000
-	);
-	camera.position.z = 5;
-	scene.add(camera);
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.x = 1;
+  camera.position.y = 3;
+  camera.position.z = 0;
+  scene.add(camera);
 
-	const light = new THREE.DirectionalLight(0xffffff, 1);
-	light.position.x = 1;
-	light.position.z = 2;
-	scene.add(light);
+  // AxesHelper: 축을 보여줌
+  const axesHelper = new THREE.AxesHelper(3); // 사이즈
+  scene.add(axesHelper);
 
-	// Mesh
-	const geometry = new THREE.BoxGeometry(1, 1, 1);
-	const material = new THREE.MeshStandardMaterial({
-		color: 'seagreen'
-	});
-	const mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
+  // GridHelper: 그리드를 보여줌
+  const gridHelper = new THREE.GridHelper(5); // 사이즈
+  scene.add(gridHelper);
 
-	// 그리기
-	const clock = new THREE.Clock();
+  // Light
+  // AmbientLight: 전체적으로 조명을 은은하게 적용
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
 
-	function draw() {
-		const time = clock.getElapsedTime();
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    color: "seagreen",
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = 2;
+  scene.add(mesh);
 
-		mesh.rotation.y = time;
+  camera.lookAt(mesh.position); // 카메라가 대상의 위치를 바라보도록 함
 
-		renderer.render(scene, camera);
-		renderer.setAnimationLoop(draw);
-	}
+  // 그리기
+  const clock = new THREE.Clock();
 
-	function setSize() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		renderer.render(scene, camera);
-	}
+  function draw() {
+    const time = clock.getElapsedTime();
 
-	// 이벤트
-	window.addEventListener('resize', setSize);
+    mesh.rotation.y = time;
 
-	draw();
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
 }
